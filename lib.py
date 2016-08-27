566add77b4bdabcb8411b7a680a9a1ff53ca4205
mangogao <jjmangogao@gmail.com>
2016-08-27 14:35:34 +0800

    update
    new features for logging.

--- lib.py
+++ lib.py
@@ -7,9 +7,37 @@ import re
 import sqlite3
 import requests
 import smtplib
+import logging
 from datetime import datetime, timedelta
 from email.mime.text import MIMEText
 from email.header import Header
+
+
+class shulib_debug(object):
+
+    def __init__(self):
+        super(shulib_debug, self).__init__()
+
+        # 日志设置
+        logging.basicConfig(
+            level=logging.INFO,
+            format='[%(asctime)s] %(message)s',
+            datefmt='%F %T',
+            filename='./shulib.log',
+            filemode='a'
+        )
+
+        self.info('日志记录启动')
+
+    def info(self, msg):
+        logging.info(msg)
+
+    def warn(self, msg, send_mail=True):
+        logging.warn(msg)
+        if send_mail:
+            shulib.send_admin(msg)
+
+
 
 
 class shulib_email(object):
@@ -17,14 +45,20 @@ class shulib_email(object):
     def __init__(self, sender):
         super(shulib_email, self).__init__()
 
+        logger.info('邮件系统启动')
         # 发送者信息
         sender_keys = ('addr', 'host', 'user', 'pass')
         self.__sender = dict(zip(sender_keys, sender))
 
         # 连接邮件服务器
-        self.__svr = smtplib.SMTP_SSL(host=self.__sender['host'])
-        self.__svr.set_debuglevel(email_debuglevel)
-        self.__svr.login(self.__sender['user'], self.__sender['pass'])
+        try:
+            self.__svr = smtplib.SMTP_SSL(host=self.__sender['host'])
+            self.__svr.set_debuglevel(email_debuglevel)
+            self.__svr.login(self.__sender['user'], self.__sender['pass'])
+        except Exception as e:
+            msg = '连接邮件服务器失败: %s' % (e)
+            logger.warn(msg, False)
+            exit()
 
     # 发送邮件
     def send(self, send_to, mail_msg):
@@ -38,7 +72,16 @@ class shulib_email(object):
         msg['To'] = send_to
 
         # 发送邮件
-        self.__svr.sendmail(self.__sender['addr'], send_to, msg.as_string())
+        try:
+            self.__svr.sendmail(self.__sender['addr'], send_to, msg.as_string())
+        except Exception as e:
+            msg = '发送邮件失败: %s' % (e)
+            logger.warn(msg, False)
+            exit()
+
+    # 发送给管理员
+    def send_admin(self, mail_msg):
+        self.send(self, self.__sender['user'], mail_msg)
 
 
 class msg_generator(object):
@@ -102,7 +145,7 @@ class msg_generator(object):
         # 将变量加入html
         funtion_vars = vars()
         keys = ('book', 'date', 'style', 'days', 'price')
-        kwargs = {key:funtion_vars[key] for key in keys}
+        kwargs = {key: funtion_vars[key] for key in keys}
         self.__msg += self.__item.format(**kwargs)
 
     @staticmethod
@@ -124,8 +167,10 @@ class msg_generator(object):
         else:
             # 大于一周时取消发送邮件
             title = '您借阅的图书还有%d天到期。' % (self.__min_days)
+            logger.info(title)
             return None
 
+        logger.info(title)
         self.__msg += self.__msg_end
         return self.__msg.format(title=title)
 
@@ -134,6 +179,7 @@ def query(user_info):
 
     # 参数
     [userid, password, send_to] = list(user_info)
+    logger.info('query(%s)' % (userid))
 
     # 会话
     session = requests.Session()
@@ -151,13 +197,21 @@ def query(user_info):
     }
 
     # 登录
-    login_request = session.post(
-        url=login_url,
-        data=login_data,
-        headers=login_headers
-    )
+    try:
+        login_request = session.post(
+            url=login_url,
+            data=login_data,
+            headers=login_headers
+        )
+    except Exception as e:
+        msg = '用户%s登录出错: %s' % (userid, e)
+        logger.warn(msg)
+        return
+
     if '登录失败' in login_request.content.decode('utf-8'):
-        print('Login Failed.')
+        msg = '用户%s登录失败: %s' % (userid)
+        logger.warn(msg)
+        return
 
     # 获取借阅页面
     html_content = session.get(loan_url).content.decode('utf-8')
@@ -170,9 +224,11 @@ def query(user_info):
 
     # 检测异常
     if not len(book_info) == len(date_info) == len(price_info):
+        msg = '检测到用户%s异常状态' % (userid)
         for i in ('book', 'date', 'price'):
             var = '%s_info' % i
-            print('[%s]' % var, vars()[var])
+            msg += '[%s] %s' % (var, repr(vars()[var]))
+        logger.warn(msg)
 
     # 生成邮件正文
     gen = msg_generator()
@@ -182,6 +238,7 @@ def query(user_info):
 
     # 发送邮件
     if not html == None:
+        logger.info('确认发送邮件')
         shulib.send(send_to, html)
 
 
@@ -191,23 +248,41 @@ if __name__ == '__main__':
     login_url = index_url + '/wap/login.php'
     loan_url = index_url + '/wap/showloan.php'
 
+    # 日志管理
+    logger = shulib_debug()
+
     # 数据库
-    db_name = 'shulib.db'
-    con = sqlite3.connect(db_name)
-    cur = con.cursor()
+    db_name = './shulib.db'
+    try:
+        con = sqlite3.connect(db_name)
+        cur = con.cursor()
+    except Exception as e:
+        msg = '数据库连接失败: %s' % (e)
+        logger.warn(msg)
+        exit()
 
     # 时差
     UTC_to_China = timedelta(hours=8)
 
     # 获取sender
     query_sql = "select * from sender;"
-    cur.execute(query_sql)
-    sender = cur.fetchall()[0]
+    try:
+        cur.execute(query_sql)
+        sender = cur.fetchall()[0]
+    except Exception as e:
+        msg = '获取sender失败: %s' % (e)
+        logger.warn(msg)
+        exit()
 
     # 获取待查询任务
     query_sql = "select * from shulib;"
-    cur.execute(query_sql)
-    target = cur.fetchall()
+    try:
+        cur.execute(query_sql)
+        target = cur.fetchall()
+    except Exception as e:
+        msg = '获取待查询任务失败: %s' % (e)
+        logger.warn(msg)
+        exit()
 
     # 邮件系统
     email_debuglevel = 0
